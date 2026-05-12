31639dbdd237f0f015a1cf46f991c1e164a16547
Haris Mohamed <harismb208@gmail.com>
2026-05-12 10:19:16 +0530

    fix: add axios interceptor to automatically attach JWT token to all requests

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,4 +7,18 @@ const API = axios.create({
   },
 });
 
+// Add a request interceptor to attach the token
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default API;
